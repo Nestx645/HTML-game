98de85ae008753dbea205eb05c8e14cbb51c4ffa
Nestx <72768267+Nestx645@users.noreply.github.com>
2021-02-15 20:55:15 +0100

    Add files via upload

---
+++ function.js
@@ -0,0 +1,35 @@
+function cacher_lvl1() {
+    zone_a_cacher = document.getElementById("text_level1");
+    zone_a_cacher.className="invisible";
+    button = document.getElementById("button_next_lvl1");
+    button.className="button_next";
+    title = document.getElementById("title_level_lvl1");
+    title.className="check";
+    title = document.getElementById("function");
+    title.className="check";
+}
+function cacher_lvl2() {
+    zone_a_cacher = document.getElementById("button_lvl2");
+    zone_a_cacher.className="invisible";
+    zone_a_cacher = document.getElementById("button_come_back_lvl2");
+    zone_a_cacher.className="invisible";
+    button = document.getElementById("button_next_lvl2");
+    button.className="button_next";
+    title = document.getElementById("title_level_lvl2");
+    title.className="check";
+}
+function level_3(){
+    reponse = document.getElementById("secret_code");
+    bonne_reponse = reponse.value
+    if (bonne_reponse=="698"){
+        zone_a_cacher = document.getElementById("button_come_back_lvl3");
+        zone_a_cacher.className="invisible";
+        button = document.getElementById("button_next_lvl3");
+        button.className="button_next";
+        title = document.getElementById("title_level_lvl3");
+        title.className="check";
+    } else {
+        alert("mauvais code sorry");
+    }
+    
+}
